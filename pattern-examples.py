4c872b77cd2d64ae193ca072bdfbe20187dea18a
mbpjc <james@jamescampbell.us>
2015-05-17 17:17:04 -0400

    working code to search twitter using pattern
    This example now can query twitter from the command line by running
    python pattern-examples.py 'search terms'

--- pattern-examples.py
+++ pattern-examples.py
@@ -2,5 +2,36 @@
 # author: James Campbell
 # Date Created: 2015 05 17
 
-from pattern.web import Twitter, plaintext
+import sys
+from termcolor import colored
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def gettweets(tweetlist, searchterms):
+	from pattern.web import Twitter, plaintext
+	twitter = Twitter(language='en') 
+	for tweet in twitter.search(searchterms, cached=False):
+		tweetlist.append(plaintext(tweet.text))
+	return tweetlist
+
+
+tweetlist = []
+
+searchterms = '"@jamescampbell"'
+if len(sys.argv) >= 2:
+	searchterms = sys.argv[1]
+
+outputtweets = gettweets(tweetlist,searchterms)
+print colored('Twitter search example using pattern package:\n Note: @jamescampbell default search term and first result returned...\n\n','blue')
+print outputtweets[1]
+print '\n\n'
+exit()
 
