cd7e086c6ebee7ebacd283fe9e6cb87c73644b7e
mbpjc <james@jamescampbell.us>
2015-05-28 22:24:54 -0400

    added tuple increment example

---
+++ tuple-example.py
@@ -0,0 +1,21 @@
+# tuple sort example
+# author: James Campbell
+# date: 2015-05-28
+
+valued = []
+lettered = []
+plusone = []
+listed = [(('d',0),('g',0)),(('d',0),('d',1)),(('i',0),('g',0))]
+for (x,y) in listed:
+	for subx, suby in x,y:
+		valued.append(int(suby))
+		lettered.append(subx)
+for value in valued:
+	value = value + 1
+	plusone.append(int(value))
+#print plusone
+coolness = zip(lettered,plusone)
+print coolness
+
+print map(list,zip(lettered,plusone))
+exit()
