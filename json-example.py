a2bd77bd507f324582ecd1b6311af032bd037d51
mbpjc <james@jamescampbell.us>
2015-06-09 10:15:12 -0400

    added json iteration example

--- json-example.py
+++ json-example.py
@@ -7,5 +7,5 @@ jsontestdata = '{"a":"red","b":"orange","c":"blue"}'
 loadedjson = json.loads(jsontestdata)
 
 for key,value in loadedjson.items(): # use .iteritems() if python 2.7
-	print value
+	print (value)
 exit()
