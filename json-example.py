ed4f5d5e14b46f29d23031b96cfa5a11cad49088
mbpjc <james@jamescampbell.us>
2015-06-09 10:19:31 -0400

    added second iterator example

--- json-example.py
+++ json-example.py
@@ -6,6 +6,12 @@ jsontestdata = '{"a":"red","b":"orange","c":"blue"}'
 
 loadedjson = json.loads(jsontestdata)
 
+print('iterator metod')
 for key,value in loadedjson.items(): # use .iteritems() if python 2.7
 	print (value)
+
+# to show that this works as well instead of using the iterator above
+print('old school method')
+for value in loadedjson:
+	print(loadedjson[value])
 exit()
