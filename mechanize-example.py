591291a5cfd9763b59cb50f13d065832d7287d5b
mbpjc <james@jamescampbell.us>
2015-07-03 08:35:25 -0400

    added spreadsheet example

--- mechanize-example.py
+++ mechanize-example.py
@@ -10,7 +10,6 @@ br=mechanize.Browser()
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 br.open('http://www.jamescampbell.us')
 linklists = br.links()
-print(linklists)
 print ('Total links found: ' + str(len(list(linklists))))
 print list(linklists)
 for link in br.links():
---
+++ read-spreadsheet-example.py
@@ -0,0 +1,33 @@
+# read values from a spreadsheet
+
+import sys, os, subprocess, fnmatch
+import xlrd
+
+def find_files(directory, pattern):
+    for root, dirs, files in os.walk(directory):
+        for basename in files:
+            if fnmatch.fnmatch(basename, pattern):
+                filename = basename
+                yield filename
+
+filename='test.xls'
+header = 1
+outputfilename = ''
+pathoffile = ''
+
+workbook = xlrd.open_workbook(filename)
+worksheet = workbook.sheet_by_name('Sheet1')
+num_rows = worksheet.nrows - 1
+num_cells = worksheet.ncols - 1
+if header == 1:
+	curr_row = 0
+else:
+	curr_row = -1
+curr_cell = -1
+while curr_row < num_rows:
+	curr_row += 1
+	row = worksheet.row(curr_row)
+	print 'Row:', curr_row
+	outputfilename = worksheet.cell_value(curr_row, 0)
+	pathoffile = worksheet.cell_value(curr_row, 1)
+	print ('Filename: %s\nPath: %s' % (outputfilename, pathoffile))
