82ee21c9ec2d50806acf1ca39f7522cd70a632ce
James Campbell <jc@notairlab.local>
2015-11-16 22:41:33 -0500

    added tweet list test

--- sentiment-analysis-nltk-example.py
+++ sentiment-analysis-nltk-example.py
@@ -10,20 +10,28 @@ pos_tweets = [('I love this car', 'positive'),
               ('This view is amazing', 'positive'),
               ('I feel great this morning', 'positive'),
               ('I am so excited about the concert', 'positive'),
-              ('He is my best friend', 'positive')]
+              ('He is my best friend', 'positive'),
+              ('Going well','positive'),
+              ('Thank you','positive'),
+              ('Hope you are doing well','positive'),
+              ('I am very happy','positive'),
+              ('Good for you','positive')]
 
 neg_tweets = [('I do not like this car', 'negative'),
               ('This view is horrible', 'negative'),
               ('I feel tired this morning', 'negative'),
               ('I am not looking forward to the concert', 'negative'),
-              ('He is my enemy', 'negative')]
-
+              ('He is my enemy', 'negative'),
+              ('I am a bad boy','negative'),
+              ('This is not good','negative'),
+              ('I am bothered by this','negative'),
+              ('I am not connected with this','negative'),
+              ('Sadistic creep you ass. Die.','negative')]
 
 tweets = []
 for (words, sentiment) in pos_tweets + neg_tweets:
     words_filtered = [e.lower() for e in words.split() if len(e) >= 3]
     tweets.append((words_filtered, sentiment))
-
 
 def get_words_in_tweets(tweets):
     all_words = []
@@ -48,7 +56,19 @@ word_features = get_word_features(get_words_in_tweets(tweets))
 training_set = nltk.classify.apply_features(extract_features, tweets)
 classifier = nltk.NaiveBayesClassifier.train(training_set)
 
-tweet = 'Larry is my enemy'
-print (classifier.classify(extract_features(tweet.split())) )
-exit()
+runtweets = [] # setup to import a list of tweets here if you wish into a python list
+if len(sys.argv) > 1: # if parameter passed from command line for name of text file with list of tweets
+    tweetfile = sys.argv[1]
 
+runtweets.append('I am a bad boy') # test tweet incase 
+poscount = 0
+negcount = 0
+for tweett in runtweets:
+	valued = classifier.classify(extract_features(tweett.split()))
+	print (valued)
+	if valued == 'negative':
+		negcount = negcount + 1
+	else:
+		poscount = poscount + 1
+print ('Positive count: %s \nNegative count: %s' % (poscount,negcount))
+exit()
