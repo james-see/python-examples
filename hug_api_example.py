519d7db92289c242518f1b8b8e2ae510b20ead69
James Campbell <james@jamescampbell.us>
2016-03-08 12:22:42 -0500

    updated hug api example to use redis routing too

--- hug_api_example.py
+++ hug_api_example.py
@@ -2,9 +2,25 @@
 # install pip3 install hug --upgrade
 # A basic (single function) API written using Hug
 import hug
+import redis
+# import MySQLdb
+
+r = redis.StrictRedis(host='127.0.0.1',port=6379)
 
 @hug.get('/happy_birthday')
 
 def happy_birthday(name, age:hug.types.number=1):
     """Says happy birthday to a user"""
     return "Happy {age} Birthday {name}!".format(**locals())
+
+@hug.get('/redis_call')
+def a_redis_call(rname, ape=1):
+    if ape == 1:
+        return "no valid api key specified, nice try though".format(**locals())
+    if r.sismember('ape',ape) != 1:
+        return "no valid api key specified, nice try though".format(**locals())
+    else:
+        coolness = r.get(rname).decode('utf8')
+        r.decr(ape)
+        numleft = int(r.get(ape))
+        return "Authenticated using api key {ape}. You have {numleft} queries left. This is the {rname} value you requested: {coolness}".format(**locals())
