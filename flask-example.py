d6b8c3b592f2ccb2aa7483208520f9ada9ecd63c
James Campbell <james@jamescampbell.us>
2016-06-08 09:31:54 -0400

    updated example server with custom port option and path check

---
+++ flask-example.py
@@ -0,0 +1,9 @@
+"""
+Author: James Campbell
+Date: Mon May 23 16:26:36 2016
+Date Updated:
+What is this code: An example Flask connection
+Why?: For me to remember later
+"""
+
+
--- server-example.py
+++ server-example.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python
 # Author: James Campbell
 # Date: May 23rd 2016
-# Date Updated:
+# Date Updated: June 8th 2016
 # What: Starts a http server as an example (works in Python 3)
 import time
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
+# default unless set at prompt
 hostPort = 10010
 
+customPort = input("Default port 10010, hit enter or type custom one now: ")
+if customPort != '': hostPort = int(customPort)
 hostName = "localhost"
 
 class MyServer(BaseHTTPRequestHandler):
@@ -16,13 +19,16 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(bytes("<html><head><title>Title goes here.</title></head>", "utf-8"))
-        self.wfile.write(bytes("<body><p>This is a test.</p>", "utf-8"))
-        self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body style='font-family:monospace;'><p>This is a test.</p>", "utf-8"))
+        # you can use if else to check path and do custom things based on path accessed
+        if self.path == '/win': self.wfile.write(bytes("<p>YOU WIN! @ path %s</p>" % self.path, "utf-8"))
+        else: self.wfile.write(bytes("<p>You accessed path: %s</p>" % self.path, "utf-8"))
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
 myServer = HTTPServer((hostName, hostPort), MyServer)
 print(time.asctime(), "Server Starts - %s:%s" % (hostName, hostPort))
 
+# continue to serve until a keypress in terminal
 try:
     myServer.serve_forever()
 except KeyboardInterrupt:
