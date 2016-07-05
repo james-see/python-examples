d849afd5bd02786111a39c62700cc1578a7c4619
jamesacampbell <james@jamescampbell.us>
2016-07-04 20:42:58 -0400

    updated rethinkdb

--- rethinkdb-example.py
+++ rethinkdb-example.py
@@ -2,14 +2,22 @@
 """
 Author: James Campbell
 Date: July 3rd 2016
-Date Updated:
-What: RethinkDB python example
-More: https://rethinkdb.com/api/python/
+Date Updated: July 4th 2016
+What: RethinkDB python example create and put data into it from pastebin site.
+Documentation on RethinkDB: https://rethinkdb.com/api/python/
 """
 import rethinkdb as r
+import urllib.request
+import urllib
+
+urltograb = 'http://psbdmp.com/api/dump/daily'
+tables = ['hellopaste']
 r.connect('localhost', 28015).repl()
-if r.db('test').table('tv_shows')
-#r.db('test').table_create('tv_shows').run()
+for table in tables: 
+	if not rdb.db(self.db).table_list().contains(table).run(): 
+		rdb.db(self.db).table_create(table).run()
+
+
 #r.table('tv_shows').insert({ 'name': 'Star Trek TNG' }).run()
 tv_shows = r.table('tv_shows').pluck('name').run()
 print(tv_shows)
