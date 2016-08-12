9675013d7351f2c81281996f7405e874517aae2b
jamesacampbell <james@jamescampbell.us>
2016-08-11 21:56:56 -0400

    updated fuzzywuzzy

--- fuzzywuzzy-example.py
+++ fuzzywuzzy-example.py
@@ -1,6 +1,11 @@
+# Author: James Campbell
+# What: Example to use fuzzywuzzy which does a fuzzy match fast
+# Date: August 11th 2016
+
 from fuzzywuzzy import fuzz
 from fuzzywuzzy import process
 
 choices = ["Atlanta Falcons", "New York Jets", "New York Giants", "Dallas Cowboys"]
 hello = process.extractOne("cowboys", choices)
-print(hello)
+print(hello) # should print Dallas Cowboys
+
