0ac85bd6719a05bc9f669632a914eb4f62f311c1
jamesacampbell <james@jamescampbell.us>
2016-11-02 22:37:45 -0400

    added pretty print tables

--- google-result-counts.py
+++ google-result-counts.py
@@ -1,8 +1,9 @@
-
+import re
 import time
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from tabulate import tabulate
 
 diction = []
 subset = []
@@ -18,6 +19,8 @@ with open(fname) as f:
 #args = parser.parse_args()
 tsubset = tqdm(subset,total=len(subset))
 report = ' domain name   |   total number of hits \n-------------------------------------------\n'
+reporter = {"site_name":[],"results":[]}
+totaler = 0 # store total results
 for site in tsubset:
 	tsubset.set_description("Processing %s" % site)
 	r = requests.get('http://www.google.com/search',
@@ -26,7 +29,14 @@ for site in tsubset:
 	                )
 
 	soup = BeautifulSoup(r.text, 'html.parser')
-	time.sleep(3)
-	report = report+site+' '+soup.find('div',{'id':'resultStats'}).text+'\n'
-print(report)
+	time.sleep(1)
+	reporter["site_name"].append(site)
+	results = soup.find('div',{'id':'resultStats'}).text
+	reporter["results"].append(results)
+	# report = report+site+' '+results+'\n'
+	try: totaler = totaler + int(re.sub("[^0-9]", "", results))
+	except: totaler = totaler
+#print(report) # old table format
+print tabulate(reporter,headers="keys",tablefmt="pipe") # awesome using tabulate
+print('total pages: {}'.format(totaler))
 exit("finis.")
