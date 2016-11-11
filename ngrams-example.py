4757d0f313f4e3e3b63829870f24f64a76abccfa
J C <james@jamescampbell.us>
2016-11-11 09:37:30 -0500

    Update ngrams-example.py

--- ngrams-example.py
+++ ngrams-example.py
@@ -8,10 +8,9 @@ from nltk.util import ngrams
 import re, string
 
 def get_ngrams(text, n ):
-    l = word_tokenize(text)
+    l = word_tokenize(text,n)
     ll = [x for x in l if not re.fullmatch('[' + string.punctuation + ']+', x)]
-    n_grams = ngrams(word_tokenize(ll), n)
-    return [ ' '.join(grams) for grams in n_grams]
+    return [ ' '.join(grams) for grams in ll]
 
 ngramer = get_ngrams("This is a sentence to parse out ngrams for it.",4)
 for gram in ngramer:
