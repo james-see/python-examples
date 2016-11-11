893d16c506930b754808a1823f1d07ce2c8321aa
J C <james@jamescampbell.us>
2016-11-11 09:20:45 -0500

    Update ngrams-example.py

--- ngrams-example.py
+++ ngrams-example.py
@@ -10,4 +10,5 @@ def get_ngrams(text, n ):
     n_grams = ngrams(word_tokenize(text), n)
     return [ ' '.join(grams) for grams in n_grams]
 
-get_ngrams("This is a sentence to parse out ngrams for it.",4)
+ngramer = get_ngrams("This is a sentence to parse out ngrams for it.",4)
+print(ngramer)
