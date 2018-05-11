b8888eba4bbded544d85b75a982e519a475deb63
James Campbell <james@jamescampbell.us>
2018-05-10 23:05:13 -0400

    got chrome headless fully working for profile data

--- chrome-headless-example.py
+++ chrome-headless-example.py
@@ -1,14 +1,41 @@
 from selenium import webdriver
 
-#set a headless browser
-options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--no-sandbox')
-driver = webdriver.Chrome(chrome_options=options)
-driver.get('https://www.instagram.com/jamesanthonycampbell/')
-print(driver.title)
-#print(driver.executeScript('window._sharedData.entry_data.ProfilePage[0].graphql.user.id'))
-#print(driver._sharedData.entry_data.ProfilePage[0].graphql.user.id)
-#print(driver.console.dir("window._sharedData.entry_data.ProfilePage[0].graphql.user.id"))
-# element = driver.execute_script("return $(window._sharedData.entry_data.ProfilePage[0].graphql.user.id)") # doesn't work but maybe close?
-# print(element)
+# lets ask the user for an instagram user to look up
+# globals
+instauser = "default"
+
+# functions
+
+
+def getuser():
+    """Get instagram username."""
+    instauser = input('What instagram user to get information about? ["q" for quit]: ')
+    return instauser
+
+
+def getcontent(instauser):
+    """Headless chrome gets the content we need."""
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    driver = webdriver.Chrome(chrome_options=options)
+    driver.get('https://www.instagram.com/{}/'.format(instauser))
+    print(driver.title)
+    userdata = driver.execute_script("return _sharedData.entry_data.ProfilePage[0].graphql.user")
+    # print all of the data
+    print(f"User ID: {userdata['id']}\nBiography: {userdata['biography']}\nFriends: {userdata['edge_followed_by']['count']}\nFollowing: {userdata['edge_follow']['count']}")
+
+
+def main():
+    """Run the program."""
+    global instauser
+    while instauser != 'q':
+        instauser = getuser()
+        if instauser != 'q':
+            getcontent(instauser)
+        else:
+            print('Thanks for playing.')
+            exit()
+
+if __name__ == "__main__":
+    main()
