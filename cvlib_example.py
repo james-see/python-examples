0ca328af0a97d2f494e0e6ff2cc9963327b88edb
James Campbell <jcampbell@primer.ai>
2019-06-03 23:17:57 -0400

    updates

--- cvlib_example.py
+++ cvlib_example.py
@@ -4,8 +4,9 @@ try: import cvlib as cv
 except: exit('must pip3 install cvlib first')
 from cvlib.object_detection import draw_bbox
 import cv2
-img = image = cv2.imread('assets/still-life.jpg')
-bbox, label, conf = cv.detect_common_objects(img, model='yolov3')
+img = image = cv2.imread('assets/sv.jpg')
+bbox, label, conf = cv.detect_common_objects(img, model='largess')
+print(label)
 
 output_image = draw_bbox(img, bbox, label, conf)
 cv2.imwrite("cvlib-example-out.jpg", output_image)
