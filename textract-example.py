574259345d780b30f98210f7df9722a2c0c179ea
James Campbell <jcampbell@primer.ai>
2019-06-03 23:35:04 -0400

    add examples for tika extract text from pdf and textract

---
+++ textract-example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author: James Campbell
+# Date: June 3rd 2019
+# What: get text from a PDF
+import sys
+import textract
+text = textract.process(f"{sys.argv[1]}")
+print(text)
---
+++ tika-get-text-example.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author: James Campbell
+# Date: June 3rd 2019
+# What: get text from a PDF
+from tika import parser
+import sys
+raw = parser.from_file(f"{sys.argv[1]}")
+print(raw)
