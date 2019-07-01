7c50decf6ad9fba2e9af0389b4ff943c67006566
James Campbell <jcampbell@primer.ai>
2019-07-01 11:28:41 -0400

    More ignore

--- instagram_geo-example.py
+++ instagram_geo-example.py
@@ -1,44 +1,48 @@
-# python example to connect and retrieve values from foursquare API then 
+# python example to connect and retrieve values from foursquare API then
 # the example gets instagram users associated with that location's lat/long
-import urllib2 # needed to open url
-import urllib # needed to do urlencode
-import json # the API call returns JSON formatted data
-import datetime as dt # need date in v= as YYYYMMDD or the api call doesn't work
+# Facebook killed Instagram's API so the second part does not work at all
+# Date Updated: 1 July 2019
+import datetime as dt  # need date in v= as YYYYMMDD
+import json  # the API call returns JSON formatted data
+import urllib  # needed to do urlencode
+import urllib.parse
+import urllib.request
 # globals Foursquare API
-limiter = '1' # make sure to keep as string due to concat issues otherwise
-cityer = raw_input('What city do you want to search in? (no spaces, include country): ')
-queryer = urllib.quote(raw_input('What is the name of the venue to search?: '))
+limiter = '1'  # make sure to keep as string due to concat issues otherwise
+cityer = input('What city to search? (no spaces, include country): ')
+queryer = urllib.parse.quote(input('What is the name of venue to search?: '))
 clientider = '5HIGYBY4D24FXGCYTMYBUBGLYQSLORV03CRUS4E53F3GZ1VS'
 clientsecreter = 'B11MC3TFDEY10XQQTUDQGGTKDGCCJBOHD4RPY5VYEW12ZNIN'
-dater = dt.datetime.today().strftime("%Y%m%d") # the v needs YYYYMMDD format
+dater = dt.datetime.today().strftime("%Y%m%d")  # the v needs YYYYMMDD format
 # the actual api call url
-foursquareapivenuesearch = 'https://api.foursquare.com/v2/venues/search?limit='+limiter+'&near='+cityer+'&query='+queryer+'&client_id='+clientider+'&client_secret='+clientsecreter+'&v='+dater
-request = urllib2.urlopen(foursquareapivenuesearch) # open the url
-dataconvert = json.loads(request.read()) # read the data returned from url
-# test print(dataconvert['response']) # print list of return json
-lat = str(dataconvert['response']['venues'][0]['location']['lat']) # foursquare response latitude
-lng = str(dataconvert['response']['venues'][0]['location']['lng']) # foursquare response longitude
-print('\nLat/Long for %s: %s, %s \n' % (urllib.unquote(queryer),lat,lng)) # print lat long only
+foursquareapivenuesearch = f"https://api.foursquare.com/v2/venues/search?limit={limiter}&near={cityer}&query={queryer}&client_id={clientider}&client_secret={clientsecreter}&v={dater}"
+request = urllib.request.urlopen(foursquareapivenuesearch)  # open the url
+dataconvert = json.loads(request.read())  # read the data returned from url
+lat = str(dataconvert['response']['venues'][0]['location']['lat'])
+lng = str(dataconvert['response']['venues'][0]['location']['lng'])
+print(f'\nLat/Long for {urllib.parse.unquote(queryer)}: {lat}, {lng} \n')
 
-# now take the lat/long and plug it into instagram's api for big win / beard wax
+# now take the lat/long and plug it into instagram's api for big win
 # note: would probably turn this into a function to make it noice and modular
 
+# INSTAGRAM API KILLED BY FACEBOOK - FUCK YOU FACEBOOK!
+print("This example was killed by Facebook killing API's. Complain to Facebook.")
 # globals Instagram API
-instaclientid = '35b999a6d51344cc98ebb061da538999'
-instaaccess_token='290277.35b999a.e2423222efa04c058b0e9b95cbf77c07'
-instalat = lat # bring in var from foursquare call
-instalong = lng # bring in var from foursquare call
-instacount = '50'
-instageosearch = 'https://api.instagram.com/v1/media/search?count='+instacount+'&lat='+instalat+'&lng='+instalong+'&access_token='+instaaccess_token
-instarequest = urllib2.urlopen(instageosearch)
-instadataconvert = json.loads(instarequest.read())
-# test print(instadataconvert) # raw json return from instagram search api
-# test print str(instadataconvert['data'][0]['user']) # print first user found in json
-print ('Instagram users found at that location:\n')
-i = 0
-for instauser in instadataconvert['data']: # data is the main json object in instagram's callback
-	username = instauser['user']['username'] # 'user' is the main json object in the data array
-	i = i + 1
-	print (username)
-print ('Total users found: %s' % (i))
-exit('\n\"It is not clear that intelligence has any long-term survival value - Stephen Hawking\"\n') # always end on a high note ;)
+# instaclientid = '35b999a6d51344cc98ebb061da538999'
+# instaaccess_token='290277.35b999a.e2423222efa04c058b0e9b95cbf77c07'
+# instalat = lat # bring in var from foursquare call
+# instalong = lng # bring in var from foursquare call
+# instacount = '50'
+# instageosearch = 'https://api.instagram.com/v1/media/search?count='+instacount+'&lat='+instalat+'&lng='+instalong+'&access_token='+instaaccess_token
+# instarequest = urllib2.urlopen(instageosearch)
+# instadataconvert = json.loads(instarequest.read())
+# # test print(instadataconvert) # raw json return from instagram search api
+# # test print str(instadataconvert['data'][0]['user']) # print first user found in json
+# print ('Instagram users found at that location:\n')
+# i = 0
+# for instauser in instadataconvert['data']: # data is the main json object in instagram's callback
+# 	username = instauser['user']['username'] # 'user' is the main json object in the data array
+# 	i = i + 1
+# 	print (username)
+# print ('Total users found: %s' % (i))
+# exit('\n\"It is not clear that intelligence has any long-term survival value - Stephen Hawking\"\n') # always end on a high note ;)
--- json-example.py
+++ json-example.py
@@ -1,23 +1,25 @@
-# !/usr/bin/python3
-
+"""Example on how to use json."""
+# Date Updated: 1 July 2019
 import json
-import timeit
-
 jsontestdata = '{"a":"red","b":"orange","c":"blue"}'
-
 loadedjson = json.loads(jsontestdata)
 
+
 def itera():
-	print('iterator method')
-	for key,value in loadedjson.items(): # use .iteritems() if python 2.7
-		print (value)
+    """Quick method iterator."""
+    print('iterator method')
+    for _, value in loadedjson.items():  # use .iteritems() if python 2.7
+        print(value)
 
 # to show that this works as well instead of using the iterator above
-def iteratoo():
-	print('old school method')
-	for value in loadedjson:
-		print(loadedjson[value])
 
-if __name__ == '__main__':
-	print(timeit.timeit("itera()",setup="from __main__ import itera"))
-exit()
+
+def iteratoo():
+    """Old school method."""
+    print('old school method')
+    for value in loadedjson:
+        print(loadedjson[value])
+
+
+itera()
+iteratoo()
