31734470654f083388d2f9303e18e238b7e1b52b
James Campbell <jcampbell@primer.ai>
2019-07-01 12:03:07 -0400

    yo recipes

--- mechanize-example.py
+++ mechanize-example.py
@@ -1,17 +1,13 @@
-
-# mechanize-example.py
-# example showing how you can use mechanize to loop through links on a domain
-
+"""Mechanize to loop through links on a domain."""
+# Date Updated: 1 July 2019
+# Author: James Campbell
 import mechanize
-import sys
-import re
 
-br=mechanize.Browser()
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-br.open('http://www.jamescampbell.us')
+br = mechanize.Browser()
+br.addheaders = [("User-agent", "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1")]
+br.open("http://www.jamescampbell.us")
 linklists = br.links()
-print ('Total links found: ' + str(len(list(linklists))))
-print list(linklists)
+print(f"Total links found: {str(len(list(linklists)))} ")
+print(list(linklists))
 for link in br.links():
-    print link.text, link.url
-exit()
+    print(link.text, link.url)
