605eea91083be74cd7aa73c5b01cb1077747041f
James Campbell <jcampbell@primer.ai>
2019-07-02 11:06:12 -0400

    wip

--- exif_reader-example.py
+++ exif_reader-example.py
@@ -1,6 +1,7 @@
-"""Show how to get exif data."""
+"""Show how to get exif data and iptc data from various libraries."""
 import sys
 from PIL import Image, ExifTags
+
 
 try:
     import exifread
@@ -8,45 +9,35 @@ except ModuleNotFoundError as e:
     print(e, "pip3 install this!")
     exit(1)
 try:
-    import iptcinfo
+    import iptcinfo3
 except ModuleNotFoundError as e:
     print(e, "pip3 install this!")
     exit(1)
 
 # Open image file for reading (binary mode)
 path_name = 'assets/cat.jpg'
-try:
-    f = sys.argv[1]  # check to see if image in command line
-    f = open(f, 'rb')
+f = sys.argv[1]  # check to see if image in command line
+f = open(f, 'rb')
 
 # Return Exif tags
 tags = exifread.process_file(f)
 totaltags = len(tags)
-print ('-------EXIF DATA FOUND-------')
-print ('Total EXIF tags found: %s' % (totaltags,))
+print('-------EXIF DATA FOUND-------')
+print(f"Total EXIF tags found: {totaltags}")
 for tag in tags.keys():
-	print ("Key: %s, value %s" % (tag, tags[tag]))
-print ('-----------------END EXIF DATA-------')
+    print("Key: %s, value %s" % (tag, tags[tag]))
+print('-----------------END EXIF DATA-------')
 
 im = Image.open(sys.argv[1])
 try:
-    iptc = iptcinfo.IPTCInfo(sys.argv[1])
-
-    image_title = iptc.data.get('object name', '') or iptc.data.get('headline', '')
-    image_description = iptc.data.get('caption/abstract', '')
-    image_tags = iptc.keywords
-    print (image_description)
-    print (image_tags)
-    print(image_title)
-
+    info = iptcinfo3.IPTCInfo(sys.argv[1])
+    print('-------IPTC DATA FOUND-------')
+    for k, v in info._data.items():
+        print(k, v)
+    info['city'] = 'poop'
+    info.save('~/iptcout.jpg')
 except Exception as e:
     if str(e) != "No IPTC data found.":
         raise
-print ('--------START OF PY3exiv2 DATA----------')
-data = py3exiv2.metadata.ImageMetadata(sys.argv[1])
-data.read()
-for key in data.exif_keys:
-    tag = data[key]
-    print(' %-40s%s' %(key, tag.value))
-print ('-------END PY3exiv2 DATA-----------')
+
 sys.exit()
